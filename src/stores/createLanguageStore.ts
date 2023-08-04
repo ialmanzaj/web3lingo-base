d6a76348d83da23c4a577e0a896d87deead2a73d
Isaac Almanza <isaac.almanza19@gmail.com>
2023-08-04 16:02:56 -0500

    fix: fix bug with language inde

--- src/stores/createLanguageStore.ts
+++ src/stores/createLanguageStore.ts
@@ -6,7 +6,7 @@ export type LanguageSlice = {
   setLanguage: (newLanguage: Language) => void;
 };
 
-const spanishLanguageIndex = 6;
+const spanishLanguageIndex = 1;
 
 export const createLanguageSlice: BoundStateCreator<LanguageSlice> = (set) => ({
   language: languages[spanishLanguageIndex],
--- src/utils/languages.ts
+++ src/utils/languages.ts
@@ -1,10 +1,8 @@
 export type Language = (typeof languages)[number];
 
 const languages = [
-
   { name: "English", nativeName: "English", viewBox: "0 0 82 66", code: "en" },
   { name: "Spanish", nativeName: "Español", viewBox: "0 66 82 66", code: "es" },
-  
   
 ] as const;
 
