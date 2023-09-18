d860465c8a6972c8f237694f3a0be9e4050b733d
Isaac Almanza <isaac.almanza19@gmail.com>
2023-09-18 17:49:41 -0300

    fix: fixed waitlist form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -56,78 +56,80 @@ const Home: NextPage = () => {
                     </p>
                     <div className="mt-10 sm:mt-12">
                       {/* This is a working waitlist form. Create your access key from https://web3forms.com/s to setup.  */}
-                      <form
-                        onSubmit={onSubmit}
-                        className="sm:mx-auto sm:max-w-xl lg:mx-0"
-                        //action="https://aprendiendodefi.us21.list-manage.com/subscribe/post?u=3ef26e10472c36d247a54389e&amp;id=a6db9a6c12&amp;f_id=00745de1f0"
-                      >
-                        <div className="sm:flex">
-                          {/* <input
-                            type="hidden"
-                            name="access_key"
-                            defaultValue="YOUR_ACCESS_KEy_HERE"
-                          />
-                          <input
-                            type="hidden"
-                            name="subject"
-                            defaultValue="New Waitlist"
-                          /> */}
-                          <div className="min-w-0 flex-1">
-                            <label htmlFor="email" className="sr-only">
-                              Email address
-                            </label>
-                            <input
-                              onChange={handleFieldChange}
-                              value={fields.EMAIL}
-                              id="EMAIL"
-                              autoFocus
-                              required
-                              pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
-                              type="email"
-                              placeholder="Introduce tu correo electrónico"
-                              className="block w-full rounded-md border-0 bg-gray-200 px-4 py-3 text-base text-gray-900 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-gray-400"
-                              defaultValue=""
-                              autoComplete="off"
-                            />
-                          </div>
-                          <div className="mt-3 sm:ml-3 sm:mt-0">
-                            <button
-                              ref={buttonRef}
-                              type="submit"
-                              className="grow rounded-2xl border-b-4 border-purple-600 bg-purple-500 p-3 font-bold text-white duration-300 ease-out hover:bg-purple-600 sm:min-w-[150px] sm:max-w-fit sm:grow-0"
-                            >
-                              Join Waitlist
-                            </button>
-                          </div>
-                        </div>
-                      </form>
-                      <div className="relative py-3">
-                        {(success || error) && (
-                          <div className="shadow-outline-gray animate-fade-bottom absolute flex items-start space-x-2 rounded-[9px] bg-purple-200 px-6 py-4 ">
+                      {success || error ? (
+                        <section className="relative w-full">
+                          <div className="shadow-outline-gray animate-fade-bottom  flex items-start space-x-2 rounded-[9px] bg-purple-200 px-6 py-4 ">
                             <div className="flex h-6 w-6 flex-shrink-0 items-center justify-center rounded-full border-purple-600 bg-purple-500">
                               🥳
                             </div>
                             <div className="text-xs sm:text-sm">
                               {success ? (
                                 <p>
-                                  Congrats! We are excited to have you here,
-                                  We&apos;ve added{" "}
-                                  <span className="font-bold">
-                                    {fields.EMAIL}
-                                  </span>{" "}
-                                  to our waitlist. We&apos;ll let you when ready
-                                  start to try it out.
+                                  ¡Felicidades! Estamos muy contentos de tenerte
+                                  aquí, Ya te hemos agregado a a nuestra lista
+                                  de espera. Te avisaremos cuando esté listo
+                                  para probarlo.
                                 </p>
                               ) : (
                                 <p>
-                                  You are already added to our waitlist.
-                                  We&apos;ll let you know when we launch!
+                                  No hemos podido añadirle a la lista de espera
+                                  Vuelva a intentarlo o escríbanos a
+                                  isaac@web3lingo.com. Gracias por su
+                                  comprensión
                                 </p>
                               )}
                             </div>
                           </div>
-                        )}
-                      </div>
+                        </section>
+                      ) : (
+                        <form
+                          onSubmit={onSubmit}
+                          className="sm:mx-auto sm:max-w-xl lg:mx-0"
+                          //action="https://aprendiendodefi.us21.list-manage.com/subscribe/post?u=3ef26e10472c36d247a54389e&amp;id=a6db9a6c12&amp;f_id=00745de1f0"
+                        >
+                          <div className="sm:flex">
+                            {/* <input
+                              type="hidden"
+                              name="access_key"
+                              defaultValue="YOUR_ACCESS_KEy_HERE"
+                            />
+                            <input
+                              type="hidden"
+                              name="subject"
+                              defaultValue="New Waitlist"
+                            /> */}
+                            <div className="min-w-0 flex-1">
+                              <label htmlFor="email" className="sr-only">
+                                Email address
+                              </label>
+                              <input
+                                onChange={handleFieldChange}
+                                value={fields.EMAIL}
+                                id="EMAIL"
+                                autoFocus
+                                required
+                                pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
+                                type="email"
+                                placeholder="Introduce tu correo electrónico"
+                                className="block w-full rounded-md border-0 bg-gray-200 px-4 py-3 text-base text-gray-900 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-gray-400"
+                                defaultValue=""
+                                autoComplete="off"
+                              />
+                            </div>
+                            <div className="mt-3 sm:ml-3 sm:mt-0">
+                              <button
+                                ref={buttonRef}
+                                type="submit"
+                                className="grow rounded-2xl border-b-4 border-purple-600 bg-purple-500 p-3 font-bold text-white duration-300 ease-out hover:bg-purple-600 sm:min-w-[150px] sm:max-w-fit sm:grow-0"
+                              >
+                                Join Waitlist
+                              </button>
+                            </div>
+                          </div>
+                        </form>
+                      )}
+
+                      <div className="relative py-3"></div>
                     </div>
                   </div>
                 </div>
